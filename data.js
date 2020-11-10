478dd68118d3dba84151118168c6745785ba406d
Korawit <614259021@webmail.npru.ac.th>
2020-11-10 21:09:06 +0700

    frist commit

---
+++ data.js
@@ -0,0 +1,87 @@
+const data = [
+    {
+        id: 1,
+        name: "หมูปลาร้าฮีโร่ - ราชดำเนินกลางเหนือ",
+        add: "ดอนเมือง",
+        price: "40.00",
+        type: "เมนูเส้น",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZN1HB3BFA5CGX/hero/aa56db09b8d940939bf9afb9bb67cdf8_1600222326842784224.jpeg"
+    },
+    {
+        id: 2,
+        name: "ยำอิเอ๋อ",
+        add: "ถนนเวียงบัว",
+        price: "100-250",
+        type: "ยำ",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYUJG6MGJ241EA/hero/ec5c277e571c4d4ab4d7eba8842c9fc8_1597828686301286902.jpeg"
+    },
+    {
+        id: 3,
+        name: "จิวโจ๊กบางลำพู",
+        add: "ถนนตานี",
+        price: "390",
+        type: "อาหารเส้น",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000001zo/hero/upload-photo-Hero_Photo_cb41e8148e50410a8f33632e0c336c02_1552533003406029094.jpeg"
+    },
+    {
+        id: 4,
+        name: "Bubble House",
+        add: "ตลาดธานินท์",
+        price: "25-30",
+        type: "เครื่องดื่ม",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZCZJTEHGEAZV6/hero/afc6ec061cf3400d914f7607a553c214_1595772212894082617.jpeg"
+    },  
+     {
+        id:5,
+        name: "Fresh Me (เฟรช มี)",
+        price: "50-75",
+        add: "มาร์เก็ตเพลส ดุสิต",
+        type: "เครื่องดื่ม",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZDXLYJCNJUKGJ/hero/4c63ccba4a2347d38c2ced21d013c2f9_1600056041276780884.jpeg"
+    },
+  
+    {
+        id: 6,
+        name: "ส้มตำ ปางสะหวัน",
+        add: " ถนนทองกวาว",
+        price: "100-250",
+        type: "ส้มตำ",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYM1HE6JLKCKLX/hero/200209624_3984a5644d16453989288723f2abf341_1557739388559101220.jpg"
+    },
+    {
+        id: 7,
+        name: "(ข้าวแกงเด็กอ้วน) ",
+        add: " ถนนจรัญสนิทวงศ์ ",
+        price: "25-40",
+        type: "ข้าวพริกแกงไก่",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY2UE3WCGYLWJA/hero/7bf40e39e7ab46249cb59ccc92541041_1571126736915913599.jpeg"
+    },
+   
+    {
+        id: 8,
+        name: "เยสครก",
+        add: "ตลาดอินดี้ปิ่นเกล้า",
+        price: "เมื่อสั่งซื้อขั้นต่ำ 300 ลด 20%",
+        type: "ส้มตำ",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/Merchants/3-CYTDEXM2MGBJET/photos/4cf4da84477d471f818cdd73ec686dde_1560325178633902002.jpg"
+    }, 
+    {
+        id: 9,
+        name: "ฟูฟูพิซซ่าญี่ปุ่น",
+        add: "ศิริธร",
+        price: "50-100",
+        type: "อาหารญี่ปุ่น",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZB2VBDCCYCUT2/hero/7d2a40e5bb6a49d08734eb1bb8db6155_1595328341154584934.jpeg"
+    },
+     {
+        id: 10,
+        name: "KFC (เคเอฟซี)",
+        add: " คลองถม",
+        price: "25-50",
+        type: "เครื่องดื่ม",
+        imageURL: "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY2UVUWFHBUUAJ/hero/2ec3103e6a53482d80e41638171db423_1604886309360342884.jpeg"
+    },
+ 
+]
+
+module.exports = data;
